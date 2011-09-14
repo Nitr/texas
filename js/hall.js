b5411d4e6bb36cd917690aa250edd9727142210f
HpyHacking <crazyflapjack@gmail.com>
2011-09-15 07:16:45 +0800

    添加对大厅列表中玩家游戏位置现实的支持

--- js/hall.js
+++ js/hall.js
@@ -1,8 +1,15 @@
 $(function() {
-  var games = [];
-  var seats = [];
-  var gid = 0;
-  var scount = 0;
+  var five_point = [{},
+    {x: 223, y: 211},
+    {x: 49, y: 148},
+    {x: 145, y: 17},
+    {x: 374, y: 17},
+    {x: 438, y: 148}
+  ];
+  
+  var games = [], seats = [];
+  var gid = 0, scount = 0;
+
   var gen_game_query = function(arr) {
     var o = {cmd: 'GAME_QUERY', game_type: 0};
     o.waiting = arr.pop();
@@ -16,6 +23,8 @@ $(function() {
   }
 
   $("#games_wrapper").setTemplateElement("games_temp");
+  $("#seats_wrapper").setTemplateElement("seats_temp");
+  $("#seats_wrapper").processTemplate({end: 8});
 
   $.pp.reg("PLAYER_INFO", function(obj) {
     $("#login").hide();
@@ -35,16 +44,22 @@ $(function() {
     $("#games_wrapper").processTemplate({datas: games});
 
     $("#games_wrapper tr").click(function() {
+      if ($(this).attr('gid') == gid)
+        return;
+
       //遍历所有的行，移除class:selected
       $.each($("#games_wrapper tr"), function(i, n) {
         $(n).removeClass("selected");
       });
+
       //给当前行添加class:selected
       $(this).addClass("selected");
       seats = [];
       gid = $(this).attr('gid');
       scount = $(this).attr('seats');
-      //$.ws.send($.pp.write({cmd: "SEAT_QUERY", gid: gid}));
+
+      $('.seat').hide();
+      $.ws.send($.pp.write({cmd: "SEAT_QUERY", gid: gid}));
     });
 
   });
@@ -60,5 +75,11 @@ $(function() {
   });
 
   $.pp.reg("SEAT_STATE", function(obj) {
+    if (obj.gid != gid)
+      return;
+
+    $('#seat' + obj.seat).show('normal').attr("style", "top: " + five_point[obj.seat].y + "px; left: " + five_point[obj.seat].x + "px;");
+    $('#seat' + obj.seat + ' > .inplay').text(obj.inplay);
+    $('#seat' + obj.seat + ' > .nick').text(obj.pid);
   });
 });
