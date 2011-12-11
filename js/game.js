b1be5739334d81c2262612433a76385eb5a797ee
HpyHacking <crazyflapjack@gmail.com>
2011-12-12 00:17:29 +0800

    使用div代替img显示gametable

--- js/game.js
+++ js/game.js
@@ -2,15 +2,19 @@ var Game;
 
 Game = (function() {
 
-  function Game(detail, dom) {
-    this.detail = detail;
+  function Game(gid, dom) {
+    this.gid = gid;
     this.dom = dom;
-    this.gid = this.detail.gid;
-    this.dom.trigger('inited');
     return;
   }
 
-  Game.prototype.add_seat = function(detail) {
+  Game.prototype.init = function(detail) {
+    this.detail = detail;
+    this.seats = [];
+    return this.dom.trigger('inited');
+  };
+
+  Game.prototype.init_seat = function(detail) {
     this.detail = detail;
   };
 
@@ -22,12 +26,8 @@ $(function() {
   var game, game_dom;
   game = null;
   game_dom = $('#game');
-  game_dom.bind('inited', function() {
-    $(this).stopTime();
-    unblockUI();
-  });
   game_dom.bind('switch_game', function(event, args) {
-    $(this).show();
+    game = new Game(args.gid, game_dom);
     switch (args.action) {
       case 'watch':
         $.ws.send($.pp.write({
@@ -46,17 +46,21 @@ $(function() {
       default:
         throw 'unknown game action';
     }
+    $(this).show();
     blockUI('#msg_joining');
     return $(this).oneTime('3s', function() {
       blockUI('#err_network');
     });
   });
+  game_dom.bind('inited', function() {
+    $(this).stopTime();
+    unblockUI();
+  });
   $.pp.reg("GAME_DETAIL", function(detail) {
-    game = new Game(detail, game_dom);
-    console.log('game_detail');
+    game.init(detail);
   });
   $.pp.reg("SEAT_DETAIL", function(detail) {
-    game.add_seat(detail);
-    console.log('seat_detail');
+    game.init_seat(detail);
   });
+  $.pp.reg("CANCEL", function(args) {});
 });
--- src/coffee/game.coffee
+++ src/coffee/game.coffee
@@ -1,44 +1,52 @@
 class Game
-  constructor: (@detail, @dom) ->
-    @gid = @detail.gid
-    @dom.trigger 'inited'
+  constructor: (@gid, @dom) ->
     return
 
-  add_seat: (@detail) ->
+  init: (@detail) ->
+    @seats = []
+    @dom.trigger 'inited'
+
+  init_seat: (@detail) ->
+    #@seats[@detail.seat] = new Seat @detail
+
     return
 
 $ ->
   game = null
   game_dom = $('#game')
 
-  game_dom.bind 'inited', ->
-    $(@).stopTime()
-    unblockUI()
-    return
-
   game_dom.bind 'switch_game', (event, args)->
-    $(@).show()
+    game = new Game args.gid, game_dom
 
     switch args.action
       when 'watch' then $.ws.send $.pp.write {cmd: "WATCH", gid: args.gid}
       when 'join' then $.ws.send $.pp.write {cmd: "JOIN", gid: args.gid, seat: 0, buyin: args.buyin}
       else throw 'unknown game action'
 
+    $(@).show()
     blockUI '#msg_joining'
 
     $(@).oneTime '3s', ->
-      # 游戏需要在3s内结束初始化，否则出错。
+      # 3s timeout show network error
       blockUI '#err_network'
       return
 
+  game_dom.bind 'inited', ->
+    $(@).stopTime()
+    unblockUI()
+    return
+
+  # {{{
   $.pp.reg "GAME_DETAIL", (detail) ->
-    game = new Game detail, game_dom
-    console.log 'game_detail'
+    game.init detail
     return
 
   $.pp.reg "SEAT_DETAIL", (detail) ->
-    game.add_seat detail
-    console.log 'seat_detail'
+    game.init_seat detail
     return
+
+  $.pp.reg "CANCEL", (args) ->
+    return
+  # }}}
 
   return
